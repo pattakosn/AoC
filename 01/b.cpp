eee9fc3d8654ed7da0cdee1274283c83e820398f
N. Pattakos <pattakosn@fastmail.com>
2024-12-02 22:52:49 +0100

    01b

--- 01/b.cpp
+++ 01/b.cpp
@@ -7,7 +7,7 @@
 
 int main() 
 {
-    const std::string fileName = "../../01/01a.txt";
+    const std::string fileName = "../../01/01.txt";
     std::ifstream file(fileName);
     if (!file.is_open()) {
         std::cerr << "Error: Could not open file " << fileName << std::endl;
@@ -29,15 +29,21 @@ int main()
         lines_read++;
     }
     std::cout << "lines_read = " << lines_read << ". col1.size = " << col1.size() << ". col2.size = " << col2.size() << "\n";
-    int distance = 0;
-    for ( int i = 0; i < lines_read; ++i) {
-        auto col1_it = col1.begin();
-        std::advance(col1_it, i);
-        auto col2_it = col2.begin();
-        std::advance(col2_it, i);
-        distance += abs(*col2_it - *col1_it);
+    int similarity = 0;
+    for ( auto const& one: col1) {
+        std::multiset<int>::iterator found_in_col2 = col2.find(one);
+        int multiplicity = 0;
+        if(found_in_col2 != col2.end()) {
+            do {
+                multiplicity++;
+                std::advance(found_in_col2, 1);
+            } while(*found_in_col2 == one);
+        }
+        std::cout << "number: " << one << " found: " << multiplicity << "times\n";
+        similarity += one * multiplicity;
+        
     }
     
-    std::cout << "Distance= " << distance << "\n";
+    std::cout << "Similarity = " << similarity << "\n";
     return EXIT_SUCCESS;
 }
