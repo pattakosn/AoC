8c8019e0aed0cd54d68b2111fd3d65bc7d25b562
N. Pattakos <pattakosn@fastmail.com>
2024-12-07 20:10:19 +0100

    01a

--- 01/a.cpp
+++ 01/a.cpp
@@ -10,7 +10,7 @@ int main()
     const std::string fileName = "../../01/01.txt";
     std::ifstream file(fileName);
     if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << fileName << std::endl;
+        std::cerr << "Error: Could not open file " << fileName << "\n";
         return EXIT_FAILURE;
     }
 
