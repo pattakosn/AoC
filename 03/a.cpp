9ef1250705f8ac433e00896172eef2edefc51ccb
N. Pattakos <pattakosn@fastmail.com>
2024-12-07 20:10:33 +0100

    03a

--- 03/a.cpp
+++ 03/a.cpp
@@ -6,14 +6,14 @@ int main() {
     const std::string fileName = "../../03/03.txt";
     std::ifstream file(fileName);
     if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << fileName << std::endl;
+        std::cerr << "Error: Could not open file " << fileName << "\n";
         return EXIT_FAILURE;
     }
 
     std::string line;
     unsigned long result = 0;
     while (std::getline(file, line)) {
-        std::cout << "Read line of size: " << line.size() << std::endl;
+        std::cout << "Read line of size: " << line.size() << "\n";
         std::regex pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
         std::match_results<std::string::const_iterator> match;
         auto start = line.cbegin();
